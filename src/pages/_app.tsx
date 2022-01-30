738d6795be9aafc910bdf65793d956ccc809bf4a
Andrej Schreiner <schreiandrej@outlook.de>
2022-01-30 13:55:32 +0100

    init commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app';
 import { Layout } from '@/components/Layout';
+import { Auth } from '@supabase/ui';
+import { supabase } from '../lib/initSupabase';
 import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Auth.UserContextProvider supabaseClient={supabase}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Auth.UserContextProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,30 @@
-export default function Home(): JSX.Element {
-  const newSet = new Set([1, 1, 2, 3, 4, 4, 5]);
-  console.log(newSet);
+import { supabase } from '../lib/initSupabase';
+import { Auth } from '@supabase/ui';
+import TodoList from '../components/TodoList';
+
+export default function Home() {
+  const { user } = Auth.useUser();
+
   return (
-    <div className="relative grid w-screen min-h-screen grid-cols-1 gap-y-3 bg-base">
-      <div className="w-full h-full bg-red-600 border border-green-600"></div>
-      <div className="grid w-full h-full grid-cols-2">
-        <div className="w-full h-full bg-red-600 border border-green-600"></div>
-        <div className="w-full h-full bg-red-600 border border-green-600"></div>
-        <div className="w-full h-full bg-red-600 border border-green-600"></div>
-        <div className="w-full h-full bg-red-600 border border-green-600"></div>
-      </div>
-      <div className="text-white">{newSet}</div>
+    <div className="w-full h-full bg-gray-300">
+      {!user ? (
+        <div className="w-full h-full flex justify-center items-center p-4">
+          <Auth supabaseClient={supabase} providers={['google', 'github']} socialLayout="horizontal" socialButtonSize="xlarge" />
+        </div>
+      ) : (
+        <div className="w-full h-full flex flex-col justify-center items-center p-4" style={{ minWidth: 250, maxWidth: 600, margin: 'auto' }}>
+          <TodoList user={supabase.auth.user()} />
+          <button
+            className="btn-black w-full mt-12"
+            onClick={async () => {
+              const { error } = await supabase.auth.signOut();
+              if (error) console.log('Error logging out:', error.message);
+            }}
+          >
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
