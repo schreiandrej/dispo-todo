d30f161bc65204ba5f13a7925f0eb26e30ed2c0b
Andrej Schreiner <schreiandrej@outlook.de>
2022-01-30 14:02:48 +0100

    updated tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
