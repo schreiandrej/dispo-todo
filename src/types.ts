dd649e995ebf191ab7d55a6b5b66a29f2810dbf9
Andrej Schreiner <schreiandrej@outlook.de>
2022-02-08 22:48:53 +0100

    added weather api type

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,57 @@ export interface ITodo {
   task: string;
   planned_day: string;
 }
+
+export interface IWeatherForcast {
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+  daily: IDaily[];
+}
+
+export interface IDaily {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  temp: ITemp;
+  feels_like: IFeelslike;
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: IWeather[];
+  clouds: number;
+  pop: number;
+  rain?: number;
+  uvi: number;
+  snow?: number;
+}
+
+export interface IWeather {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface IFeelslike {
+  day: number;
+  night: number;
+  eve: number;
+  morn: number;
+}
+
+export interface ITemp {
+  day: number;
+  min: number;
+  max: number;
+  night: number;
+  eve: number;
+  morn: number;
+}
